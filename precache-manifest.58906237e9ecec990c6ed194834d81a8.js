f4580d17af026e963c8b42ab80c806d13f1afd23
guillaume palayer <guillaume.palayer@gmail.com>
2020-07-01 01:27:13 +0200

    Updates

---
+++ precache-manifest.58906237e9ecec990c6ed194834d81a8.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06b35f215de8e633d2e3d344e14fc58",
+    "url": "/Loopring-Pay/index.html"
+  },
+  {
+    "revision": "0cf976f6f099adf02251",
+    "url": "/Loopring-Pay/static/css/main~06837ae4.f24deecd.chunk.css"
+  },
+  {
+    "revision": "196f7fd0e9255daa9a32",
+    "url": "/Loopring-Pay/static/css/main~748942c6.887f6187.chunk.css"
+  },
+  {
+    "revision": "00a2e7b3d77e771db21e",
+    "url": "/Loopring-Pay/static/css/main~970f9218.44104477.chunk.css"
+  },
+  {
+    "revision": "0cf976f6f099adf02251",
+    "url": "/Loopring-Pay/static/js/main~06837ae4.02fab2b9.chunk.js"
+  },
+  {
+    "revision": "c88b26a7caa0c1f4c316",
+    "url": "/Loopring-Pay/static/js/main~2900d54e.0489a095.chunk.js"
+  },
+  {
+    "revision": "e54b2378be75fba845fb",
+    "url": "/Loopring-Pay/static/js/main~7274e1de.390817ee.chunk.js"
+  },
+  {
+    "revision": "1ae3146c5b710db8640dc5f70c1037ee",
+    "url": "/Loopring-Pay/static/js/main~7274e1de.390817ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196f7fd0e9255daa9a32",
+    "url": "/Loopring-Pay/static/js/main~748942c6.4013d549.chunk.js"
+  },
+  {
+    "revision": "82c075caf1aa89864f814f7eedde10c8",
+    "url": "/Loopring-Pay/static/js/main~748942c6.4013d549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a2e7b3d77e771db21e",
+    "url": "/Loopring-Pay/static/js/main~970f9218.544763ee.chunk.js"
+  },
+  {
+    "revision": "d3f5f7b96f94752e92b8",
+    "url": "/Loopring-Pay/static/js/main~b5906859.4a001718.chunk.js"
+  },
+  {
+    "revision": "ce149591b1d143e74e69c0f43b7ff82e",
+    "url": "/Loopring-Pay/static/js/main~b5906859.4a001718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdf28490376a9a1da3c",
+    "url": "/Loopring-Pay/static/js/main~fdadd360.3708bad8.chunk.js"
+  },
+  {
+    "revision": "dac8e254b3ecc234ca0f28fc865690c4",
+    "url": "/Loopring-Pay/static/js/main~fdadd360.3708bad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0de2894e870fa63a35",
+    "url": "/Loopring-Pay/static/js/runtime-main.71c7c974.js"
+  },
+  {
+    "revision": "65d68bd4f2d5cd44c99ab7b30bda3b01",
+    "url": "/Loopring-Pay/static/media/Feather.65d68bd4.svg"
+  },
+  {
+    "revision": "d9c23068ef426a94453f85a8c1a695b5",
+    "url": "/Loopring-Pay/static/media/Feather.d9c23068.woff"
+  },
+  {
+    "revision": "fe1594343a6aed9427c646993d06ea9c",
+    "url": "/Loopring-Pay/static/media/Feather.fe159434.ttf"
+  },
+  {
+    "revision": "6ac726130dcb2db3245cbd2b42a47685",
+    "url": "/Loopring-Pay/static/media/avatar-group-hover-last.6ac72613.svg"
+  },
+  {
+    "revision": "672e50f73963cf97baae196cf2c7a7bd",
+    "url": "/Loopring-Pay/static/media/avatar-group-hover.672e50f7.svg"
+  },
+  {
+    "revision": "936a2d3aedfe174ffaf564808de152fe",
+    "url": "/Loopring-Pay/static/media/avatar-group.936a2d3a.svg"
+  },
+  {
+    "revision": "7ca4c2012feeb8ac3a01a078c0177b36",
+    "url": "/Loopring-Pay/static/media/avatar-status.7ca4c201.svg"
+  },
+  {
+    "revision": "66f3c3aea89e311d54dce1b0f5968d67",
+    "url": "/Loopring-Pay/static/media/cerebrisans-medium.66f3c3ae.ttf"
+  },
+  {
+    "revision": "abe53acee44a549766b43bd32c22c9ce",
+    "url": "/Loopring-Pay/static/media/cerebrisans-medium.abe53ace.woff"
+  },
+  {
+    "revision": "c01e4a7dd60f3988a32c1b87e2b05cf0",
+    "url": "/Loopring-Pay/static/media/cerebrisans-medium.c01e4a7d.eot"
+  },
+  {
+    "revision": "548f02352f33620b89e5bc6284981c36",
+    "url": "/Loopring-Pay/static/media/cerebrisans-regular.548f0235.woff"
+  },
+  {
+    "revision": "91bff5f0d5f1128401a41dc3269a684b",
+    "url": "/Loopring-Pay/static/media/cerebrisans-regular.91bff5f0.ttf"
+  },
+  {
+    "revision": "e63b280fde60f13c59b2eab17adfe03a",
+    "url": "/Loopring-Pay/static/media/cerebrisans-regular.e63b280f.eot"
+  },
+  {
+    "revision": "16e9c1c603cf10ff7ea81296e40c8c1f",
+    "url": "/Loopring-Pay/static/media/cerebrisans-semibold.16e9c1c6.woff"
+  },
+  {
+    "revision": "25aa37eee4a8a72fa7d2702b548cd843",
+    "url": "/Loopring-Pay/static/media/cerebrisans-semibold.25aa37ee.ttf"
+  },
+  {
+    "revision": "2a1663dd85b07223f419fc0e9b5abb53",
+    "url": "/Loopring-Pay/static/media/cerebrisans-semibold.2a1663dd.eot"
+  },
+  {
+    "revision": "7259dc23b16a289d01ec0b22750705d4",
+    "url": "/Loopring-Pay/static/media/header-cover.7259dc23.jpg"
+  },
+  {
+    "revision": "c971ca1909311eb4bd25cf62905f6d03",
+    "url": "/Loopring-Pay/static/media/icon-status.c971ca19.svg"
+  },
+  {
+    "revision": "67800023c9069518ebacc46bf7a518d9",
+    "url": "/Loopring-Pay/static/media/sidebar-cover.67800023.jpg"
+  },
+  {
+    "revision": "b5cc9151160ba9193c6b57016e227a57",
+    "url": "/Loopring-Pay/static/media/wechat_qrcode.b5cc9151.jpg"
+  }
+]);
